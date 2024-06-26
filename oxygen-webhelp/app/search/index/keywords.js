6304af99502f94fabc9d2db20210fc53dc15e561
iya-kho <iya.kho@gmail.com>
2024-06-26 18:49:27 +0300

    output generated

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,19 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Introduction",p:["p1"]},{w:"Steps",p:["p2"]},{w:"to",p:["p2","p4","p6"]},{w:"create",p:["p2"]},{w:"a",p:["p2"]},{w:"REST",p:["p2"]},{w:"API",p:["p2","p5"]},{w:"What",p:["p3"]},{w:"is",p:["p3"]},{w:"Ubos",p:["p3"]},{w:"Add",p:["p4"]},{w:"data",p:["p4","p7","p8","p10"]},{w:"your",p:["p4","p5","p9"]},{w:"database",p:["p4"]},{w:"with",p:["p4","p7","p8","p10"]},{w:"POST",p:["p4"]},{w:"Build",p:["p5"]},{w:"Connect",p:["p6"]},{w:"Node-RED",p:["p6","p11"]},{w:"MongoDB",p:["p6"]},{w:"Delete",p:["p7"]},{w:"Fetch",p:["p8"]},{w:"GET",p:["p8"]},{w:"Set",p:["p9"]},{w:"up",p:["p9"]},{w:"environment",p:["p9"]},{w:"Update",p:["p10"]},{w:"PUT",p:["p10"]},{w:"Common",p:["p11"]},{w:"nodes",p:["p11"]},{w:"Useful",p:["p12"]},{w:"info",p:["p12"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4, 5, 6, 7, 8, 9];
+ph["p3"]=[10, 11, 12];
+ph["p4"]=[13, 14, 5, 15, 16, 17, 18];
+ph["p5"]=[19, 15, 9];
+ph["p6"]=[20, 21, 5, 22];
+ph["p7"]=[23, 14, 17, 23];
+ph["p8"]=[24, 14, 17, 25];
+ph["p9"]=[26, 27, 15, 28];
+ph["p10"]=[29, 14, 17, 30];
+ph["p12"]=[33, 34];
+ph["p11"]=[31, 21, 32];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {7:5,3:1,6:5,2:1,5:-1,9:5,1:-1,12:-1,10:5,4:5,8:5,11:12};
